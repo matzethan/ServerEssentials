f8e87225cd7e1e52a6d0002ea9564ce87bb9598c
matzethan <matzethan@web.de>
2025-12-29 17:20:34 +0100

    v0.1 fügt einen Timer hinzu und deaktiviert während des Timer den Schaden

---
+++ src/main/java/de/matzethan/serverEssentials/ServerEssentials.java
@@ -0,0 +1,60 @@
+package de.matzethan.serverEssentials;
+
+
+import de.matzethan.serverEssentials.commands.BedwarsCommand;
+import de.matzethan.serverEssentials.commands.LobbyCommand;
+import de.matzethan.serverEssentials.commands.SMPCommand;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ServerEssentials extends JavaPlugin {
+
+    public static final String PASTEL_GREEN = "\u001B[38;2;175;225;175m";
+    public static final String PASTEL_BLUE = "\u001B[38;2;173;216;230m";
+    public static final String RESET = "\u001B[0m";
+
+    //System.out.println(PASTEL_GREEN + "" + RESET);
+
+
+
+    @Override
+    public void onEnable() {
+        this.getServer().getMessenger().registerOutgoingPluginChannel(this, "bungeecord:main");
+        this.getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
+
+        var console = Bukkit.getConsoleSender();
+        getLogger().info("");
+        console.sendMessage(PASTEL_BLUE + "_____   _____" + RESET);
+        console.sendMessage(PASTEL_BLUE + "|       |" + RESET);
+        console.sendMessage(PASTEL_BLUE + "|____   |____" + RESET);
+        console.sendMessage(PASTEL_BLUE + "    |   |" + RESET);
+        console.sendMessage(PASTEL_BLUE + "____|   |____" + RESET);
+        getLogger().info("Version: 0.1");
+        getLogger().info("Made by matzethan.");
+        getLogger().info("");
+        console.sendMessage(PASTEL_GREEN + "ServerEssentials has started successfully." + RESET);
+        getLogger().info("");
+
+        getCommand("lobby").setExecutor(new LobbyCommand(this));
+        getLogger().info("The command /lobby has been activated.");
+
+        getCommand("smp").setExecutor(new SMPCommand(this));
+        getLogger().info("The command /smp has been activated.");
+
+        getCommand("bedwars").setExecutor(new BedwarsCommand(this));
+        getLogger().info("The command /bedwars has been activated.");
+
+        getLogger().info("");
+    }
+
+    @Override
+    public void onDisable() {
+        this.getServer().getMessenger().unregisterOutgoingPluginChannel(this, "BungeeCord");
+        this.getServer().getMessenger().unregisterOutgoingPluginChannel(this, "BungeeCord");
+
+        getLogger().info("All commands have been disabled.");
+
+        getLogger().info("ServerEssentials has been stopped.");
+    }
+}
---
+++ src/main/java/de/matzethan/serverEssentials/commands/SMPCommand.java
@@ -0,0 +1,62 @@
+package de.matzethan.serverEssentials.commands;
+
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
+import de.matzethan.serverEssentials.ServerEssentials;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.jetbrains.annotations.NotNull;
+
+public class SMPCommand implements CommandExecutor {
+
+    private final ServerEssentials plugin;
+    public SMPCommand(ServerEssentials plugin) { this.plugin = plugin; }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] strings) {
+
+
+
+        if (!(commandSender instanceof Player)) {
+            commandSender.sendMessage(ChatColor.YELLOW + "Warning: This Command is for players only!");
+            return false;
+        }
+
+        Player player = (Player) commandSender;
+
+        player.setInvulnerable(true);
+
+        player.sendMessage(ChatColor.YELLOW + "You will be teleported to SMP...");
+        player.sendMessage(ChatColor.YELLOW + "3");
+
+        Bukkit.getScheduler().runTaskLater(plugin, () -> {
+            commandSender.sendMessage(ChatColor.YELLOW + "2");
+        }, 20L);
+
+        Bukkit.getScheduler().runTaskLater(plugin, () -> {
+            commandSender.sendMessage(ChatColor.YELLOW + "1");
+        }, 40L);
+
+        Bukkit.getScheduler().runTaskLater(plugin, () -> {
+            commandSender.sendMessage(ChatColor.YELLOW + "Teleporting ..");
+
+            ByteArrayDataOutput out = ByteStreams.newDataOutput();
+            out.writeUTF("Connect");
+            out.writeUTF("SMP");
+
+            player.sendPluginMessage(this.plugin, "BungeeCord", out.toByteArray());
+
+            player.setInvulnerable(false);
+        }, 60L);
+
+
+        return true;
+    }
+}
+
