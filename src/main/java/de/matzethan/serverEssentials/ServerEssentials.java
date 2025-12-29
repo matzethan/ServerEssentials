ad92d5d29a9ce9a3188c7bdd59395dd72c2cab95
matzethan <matzethan@web.de>
2025-12-29 18:42:15 +0100

    v0.1 fügt einen neuen befehl se-update um nach neuen updates zu suchen

--- src/main/java/de/matzethan/serverEssentials/ServerEssentials.java
+++ src/main/java/de/matzethan/serverEssentials/ServerEssentials.java
@@ -1,22 +1,24 @@
 package de.matzethan.serverEssentials;
 
-
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import de.matzethan.serverEssentials.commands.BedwarsCommand;
 import de.matzethan.serverEssentials.commands.LobbyCommand;
 import de.matzethan.serverEssentials.commands.SMPCommand;
+import de.matzethan.serverEssentials.commands.UpdateCommand;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable; // <-- Dieser Import hat gefehlt!
+
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public final class ServerEssentials extends JavaPlugin {
 
     public static final String PASTEL_GREEN = "\u001B[38;2;175;225;175m";
     public static final String PASTEL_BLUE = "\u001B[38;2;173;216;230m";
     public static final String RESET = "\u001B[0m";
-
-    //System.out.println(PASTEL_GREEN + "" + RESET);
-
-
 
     @Override
     public void onEnable() {
@@ -30,7 +32,7 @@ public final class ServerEssentials extends JavaPlugin {
         console.sendMessage(PASTEL_BLUE + "|____   |____" + RESET);
         console.sendMessage(PASTEL_BLUE + "    |   |" + RESET);
         console.sendMessage(PASTEL_BLUE + "____|   |____" + RESET);
-        getLogger().info("Version: 0.1");
+        getLogger().info("Version: " + getDescription().getVersion()); // Dynamisch aus plugin.yml
         getLogger().info("Made by matzethan.");
         getLogger().info("");
         console.sendMessage(PASTEL_GREEN + "ServerEssentials has started successfully." + RESET);
@@ -45,16 +47,18 @@ public final class ServerEssentials extends JavaPlugin {
         getCommand("bedwars").setExecutor(new BedwarsCommand(this));
         getLogger().info("The command /bedwars has been activated.");
 
+        getCommand("se-update").setExecutor(new UpdateCommand(this));
+        getLogger().info("The command /se-update has been activated.");
+
         getLogger().info("");
+
     }
 
     @Override
     public void onDisable() {
         this.getServer().getMessenger().unregisterOutgoingPluginChannel(this, "BungeeCord");
-        this.getServer().getMessenger().unregisterOutgoingPluginChannel(this, "BungeeCord");
 
         getLogger().info("All commands have been disabled.");
-
         getLogger().info("ServerEssentials has been stopped.");
     }
 }
---
+++ src/main/java/de/matzethan/serverEssentials/commands/UpdateCommand.java
@@ -0,0 +1,68 @@
+package de.matzethan.serverEssentials.commands;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import de.matzethan.serverEssentials.ServerEssentials;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class UpdateCommand implements CommandExecutor {
+
+    // 1. Wir deklarieren die Variable, damit wir sie überall in dieser Klasse nutzen können
+    private final ServerEssentials plugin;
+
+    public UpdateCommand(ServerEssentials plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+
+        commandSender.sendMessage(ChatColor.YELLOW + "Suche nach Updates...");
+
+        // 2. Wir geben 'plugin' (unsere Hauptklasse) an den Scheduler weiter
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+            checkForUpdatesManual(commandSender);
+        });
+
+        return true; // Sagt Bukkit, dass der Befehl erfolgreich ausgeführt wurde
+    }
+
+    // 3. Diese Methode steht AUSSERHALB der geschweiften Klammern von onCommand
+    private void checkForUpdatesManual(CommandSender commandSender) {
+        String user = "matzethan";
+        String repo = "ServerEssentials";
+
+        try {
+            URL url = new URL("https://api.github.com/repos/" + user + "/" + repo + "/releases/latest");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("User-Agent", "Paper-Plugin-UpdateChecker");
+
+            if (connection.getResponseCode() == 200) {
+                InputStreamReader reader = new InputStreamReader(connection.getInputStream());
+                JsonObject json = JsonParser.parseReader(reader).getAsJsonObject();
+
+                String latestVersion = json.get("tag_name").getAsString();
+                String currentVersion = plugin.getDescription().getVersion();
+
+                if (!currentVersion.equalsIgnoreCase(latestVersion)) {
+                    commandSender.sendMessage(ChatColor.GREEN + "Ein Update ist verfügbar: " + ChatColor.WHITE + latestVersion);
+                    commandSender.sendMessage(ChatColor.GREEN + "Download: " + ChatColor.AQUA + "https://github.com/" + user + "/" + repo + "/releases");
+                } else {
+                    commandSender.sendMessage(ChatColor.GREEN + "ServerEssentials ist aktuell! (v" + currentVersion + ")");
+                }
+            }
+        } catch (Exception e) {
+            commandSender.sendMessage(ChatColor.RED + "Fehler beim Update-Check: " + e.getMessage());
+        }
+    }
+}
